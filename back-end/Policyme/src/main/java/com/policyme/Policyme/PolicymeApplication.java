8f8e948ea284a7014d34c2642b5a96bbf61339aa
Yousuf Muhammud <muhammudy@gmail.com>
2025-10-05 20:13:15 -0500

    added backend configs

---
+++ back-end/Policyme/src/main/java/com/policyme/Policyme/PolicymeApplication.java
@@ -0,0 +1,13 @@
+package com.policyme.Policyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicymeApplication.class, args);
+	}
+
+}
