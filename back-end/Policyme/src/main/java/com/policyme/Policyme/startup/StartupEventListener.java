ab9eaa6ecce537c39e767c874dee00d3d3c9a0c0
Muhammudy <muhammudy@gmail.com>
2025-10-30 23:31:52 -0500

    cleaned the billing logic and starting working on the summary logic

--- back-end/Policyme/src/main/java/com/policyme/Policyme/startup/StartupEventListener.java
+++ back-end/Policyme/src/main/java/com/policyme/Policyme/startup/StartupEventListener.java
@@ -2,6 +2,7 @@ package com.policyme.Policyme.startup;
 
 
 import com.policyme.Policyme.service.BillService;
+import com.policyme.Policyme.service.SummaryService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.event.EventListener;
@@ -10,14 +11,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class StartupEventListener {
     public final BillService billService;
+    public final SummaryService summaryService;
 
 
-    public StartupEventListener(BillService billService) {
+    public StartupEventListener(BillService billService, SummaryService summaryService) {
         this.billService = billService;
+        this.summaryService = summaryService;
     }
 
     @EventListener(ApplicationReadyEvent.class)
     public void onApplicationRead() {
-        billService.fetchAlBillData();
+        billService.fetchAllBillData();
+//        summaryService.fetc
     }
 }
