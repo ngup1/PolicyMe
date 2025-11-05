d2195cb7f5f2b8e8f558a1b56620f3f9d8e8ef92
Muhammudy <muhammudy@gmail.com>
2025-11-04 23:49:44 -0600

    signup

---
+++ back-end/Policyme/src/main/java/com/policyme/Policyme/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.policyme.Policyme.controller;
+
+import com.policyme.Policyme.dto.ApiResponse;
+import com.policyme.Policyme.dto.SignUpRequestDTO;
+import com.policyme.Policyme.model.UserModel.AuthProvider;
+import com.policyme.Policyme.model.UserModel.User;
+import com.policyme.Policyme.repository.UserRepository;
+import com.policyme.Policyme.security.JwtUtil;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequestDTO request){
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()){ //the user already exists within the db
+            return ResponseEntity.status(409).body(new ApiResponse(false, "Email already exists"));
+        }
+        User user = User.builder()
+                .email(request.getEmail())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .authProvider(AuthProvider.NONE)
+                .passwordHash(passwordEncoder.encode(request.getPassword()))
+                .build();
+
+        userRepository.save(user);
+
+        return ResponseEntity.status(201).body(new ApiResponse(true, "User created successfully"));
+    }
+}
