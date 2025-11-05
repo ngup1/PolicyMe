1148d327668e6a8f34f84fc9a9f1ed2215f49478
Muhammudy <muhammudy@gmail.com>
2025-11-04 23:49:44 -0600

    signup

---
+++ back-end/Policyme/src/main/java/com/policyme/Policyme/dto/SignUpRequestDTO.java
@@ -0,0 +1,24 @@
+package com.policyme.Policyme.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+
+//This is for NON-OAUTH2
+@Data
+public class SignUpRequestDTO {
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @NotBlank(message = "Password is required")
+    private String password;
+    @NotBlank(message = "First name is required")
+    private String firstName;
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+}
