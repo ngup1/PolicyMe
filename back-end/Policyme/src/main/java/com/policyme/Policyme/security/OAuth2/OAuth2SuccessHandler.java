99f7b4322ee6e576e7c2c38e2d5c5ba086af86af
Muhammudy <muhammudy@gmail.com>
2025-11-05 23:11:48 -0600

    added oauth2 support

---
+++ back-end/Policyme/src/main/java/com/policyme/Policyme/security/OAuth2/OAuth2SuccessHandler.java
@@ -0,0 +1,32 @@
+package com.policyme.Policyme.security.OAuth2;
+
+import com.policyme.Policyme.security.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+
+        String userId = authentication.getName();
+        String token = jwtUtil.generateJwtToken(userId);
+
+        response.sendRedirect("http://localhost:3000/oauth-success?token=" + token); //change this later
+    }
+}
