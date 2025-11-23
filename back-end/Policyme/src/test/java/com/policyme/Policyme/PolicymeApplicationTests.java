cc73cf8a6873176ada06e15227415aa581ddabdd
Muhammudy <muhammudy@gmail.com>
2025-11-22 22:54:21 -0600

    ignore .env

--- back-end/Policyme/src/test/java/com/policyme/Policyme/PolicymeApplicationTests.java
+++ back-end/Policyme/src/test/java/com/policyme/Policyme/PolicymeApplicationTests.java
@@ -3,11 +3,14 @@ package com.policyme.Policyme;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ActiveProfiles;
 
 
 @ActiveProfiles("test")
 @SpringBootTest
+@PropertySource(value = "file:.env", ignoreResourceNotFound = true)
+
 class PolicymeApplicationTests {
 
 	@Test
